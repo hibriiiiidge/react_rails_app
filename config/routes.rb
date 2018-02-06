79506476ce75c4cf1c8e17ed169e8f922707c6cc
Yasunori Takahashi <hibriiiiidge@gmail.com>
2018-02-06 23:03:29 +0900

    Add rails initialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-
+  resources :messages, only: :index, format: 'json'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20180206111455_create_messages.rb
+++ db/migrate/20180206111455_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :content
 
       t.timestamps
